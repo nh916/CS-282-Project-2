174c0363a677c3e5fa37ad8dcb7ac8b8cf7a1765
navid <navidhariri25@gmail.com>
2019-03-17 01:57:09 -0700

    Read class with comments

--- src/Read.java
+++ src/Read.java
@@ -1,20 +1,28 @@
+/*
+Navid Hariri
+David Enaw
+*/
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Read {
 
 
-    public LinkedList <String> fileName;
+    public ArrayList <String> fileName;
     public ArrayList<String> fileData;
 
 
     public Read(String file1, String file2, String file3, String file4){
+//        initialize ArrayList
         fileData = new ArrayList<String>();
 
-        fileName = new LinkedList<String>();
+//        initialize ArrayList
+        fileName = new ArrayList<String>();
+
+//        add all files to ArrayList
         fileName.add(file1);
         fileName.add(file2);
         fileName.add(file3);
@@ -31,27 +39,36 @@ public class Read {
                 while (scan.hasNext()) {
                     fileData.add(scan.nextLine());
                 }
-            } catch (FileNotFoundException e) {
+            }
+            catch (FileNotFoundException e) {
 //            e.printStackTrace();
                 System.out.println("file not found");
-            } catch (NullPointerException e) {
+            }
+            catch (NullPointerException e) {
                 System.out.println("file error");
-            } finally {
+            }
+            finally {
                 try {
                     scan.close();
-                } catch (NullPointerException e) {
+                }
+                catch (NullPointerException e) {
                     System.out.println("file was never opened or closed");
                 }
 
             }
         }
-        System.out.println(fileData);
+//        System.out.println(fileData);
     }
 
     public static void main(String[] args){
+        /*the Read class requires at least 4 files but you can later add more if needed
+        because fileName linkedList is public
+        you can just add it to the linkedList if needed*/
+
         Read read = new Read("file1.txt", "file2.txt", "file3.txt", "file4.txt");
 
         read.loadData();
+        System.out.println(read.fileData);
         System.out.println(read.fileData.size());
     }
 
