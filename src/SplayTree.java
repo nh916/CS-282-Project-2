b726a100b614844dfc29fa477506a558f2e3379e
navid <navidhariri25@gmail.com>
2019-03-18 00:34:49 -0700

    Started on Splay and Started on AVL.
    both are very much starting and not even close to finishing.
        most of it I am not even sure if any part of it is correct or not.
    Both need to be heavily tested

--- src/SplayTree.java
+++ src/SplayTree.java
@@ -1,2 +1,62 @@
 public class SplayTree {
+    Node root;
+
+    public SplayTree(){
+        root = null;
+    }
+
+    public void insert(String toInsert){
+        insert(root, new Node(toInsert));
+    }
+
+    private void insert(Node node, Node toInsert){
+        if (node == null){
+            node = toInsert;
+            return;
+        }
+
+    }
+
+    public void remove(String toRemove){
+        remove(root, new Node(toRemove));
+    }
+
+    private void remove(Node node, Node toRemove){
+        if (root == null) {
+//            empty tree
+            return;
+        }
+        node = splay(root, key);
+
+        int cmp = key.compareTo(root.key);
+
+        if (cmp == 0) {
+            if (node.getLeft() == null) {
+                node = node.getRight();
+            }
+            else {
+                Node x = node.getRight();
+                node = node.getLeft();
+                splay(node, key);
+                root.getRight() = x;
+            }
+        }
+
+        // else: it wasn't in the tree to remove
+    }
+
+
+
+
+
+
+
+
+
+
+    public Node find(){
+
+    }
+
+
 }
