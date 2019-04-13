f38de3ed6e974c83280a36bfd97464b7d6df1eb0
navid <navidhariri25@gmail.com>
2019-04-13 14:50:12 -0700

    element cleaned

--- src/Element.java
+++ src/Element.java
@@ -8,33 +8,11 @@ public class Element implements Comparable<String> {
     }
 
 
-//    @Override
-//    public int compareTo(String element){
-////        if the new element is bigger return 1
-//        if (data.compareTo(element) > 0){
-//            return 1;
-//        }
-////        if new element is smaller return 2
-//        else if (data.compareTo(element) < 0){
-//            return -1;
-//        }
-////        if its equal to the element return 0
-//        else {
-//            return 0;
-//        }
-//    }
-
     @Override
     public int compareTo(String element){
         return data.compareTo(element);
     }
 
-
-/*    @Override
-    public int compareTo(E element){
-        Element temp = (Element) element;
-        if ()
-    }*/
 
 
     public void setData(String newData){
