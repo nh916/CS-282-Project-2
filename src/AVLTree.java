20c0c6954a0abb4bfee5b3996b4e8c8455da3507
navid <navidhariri25@gmail.com>
2019-04-13 19:09:15 -0700

    AVL tree again copied exactly from the book

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -7,172 +7,105 @@ public class AVLTree extends BST{
         root = null;
     }
 
-    @Override
-    public Node find(String findThis){
-        return super.find(root, new Node(false, false, false, false, findThis));
+
+    public Node find(String toFind){
+        return super.find(toFind);
     }
 
-    public Node insertAVL(boolean file1, boolean file2, boolean file3, boolean file4, String x){
-        return root = insert(new Node(file1, file2, file3, file4, x), root);
+    public void insertAVL(boolean file1, boolean file2, boolean file3, boolean file4, String toInsert ) {
+        root = insert(new Node(file1, file2, file3, file4, toInsert), root);
     }
 
-
-    private Node insertAVL(Node x, Node t) {
-        if (t == null) {
+    private Node insertAVL( Node x, Node t ) {
+        if( t == null ) {
             return x;
         }
 
-        int compareResult = x.getData().compareTo(t.getData());
+        int compareResult = x.compareTo(t);
 
-        if (compareResult < 0) {
-            t.setLeft(insert(x, t.getLeft()));
-            if (height(t.getLeft()) - height(t.getRight()) == 2) {
-                if (x.getData().compareTo(t.getLeft().getData()) < 0) {
-                    t = rotateWithLeftChild(t);
-                }
-                else {
-                    t = doubleWithLeftChild(t);
-                }
-            }
-        } else if (compareResult > 0) {
-            t.setRight(insert(x, t.getRight()));
-            if (height(t.getRight()) - height(t.getLeft()) == 2) {
-                if (x.getData().compareTo(t.getRight().getData()) > 0) {
-                    t = rotateWithRightChild(t);
-                } else {
-                    t = doubleWithRightChild(t);
-                }
-            }
-        } else {
-            // Duplicate; do nothing
-
+        if( compareResult < 0 ) {
+            t.setLeft(insert( x, t.getLeft() ));
+            if( height( t.getLeft() ) - height( t.getRight() ) == 2 )
+                if( x.compareTo( t.getLeft()) < 0 )
+                    t = rotateWithLeftChild( t );
+                else
+                    t = doubleWithLeftChild( t );
         }
-//        just here because the code put it here
-        t.height = Math.max( height( t.getLeft() ), height( t.getRight())) + 1;
-        return t;
-    }
-
-    private int height( Node t ) {
-        return t == null ? -1 : t.height;
-
-//        if (t == null){
-//            return -1;
-//        }
-//        else {
-//            return t.height;
-//        }
-
-    }
-
-    protected Node rotateWithLeftChild( Node k2 ) {
-        Node k1 = k2.getLeft();
-//        if (k2.getLeft() == null){
-//            return k2;
-//        }
-        k2.setLeft(k1.getRight());
-        k1.setRight(k2);
-        k2.height = Math.max(height(k2.getLeft()), height(k2.getRight())) + 1;
-        k1.height = Math.max(height(k1.getLeft()), k2.height) + 1;
-        return k1;
-
-    }
-
-    protected Node rotateWithRightChild( Node k1 ) {
-        Node k2 = k1.getRight();
-//        if (k1.getRight() == null){
-//            return k1;
-//        }
-        k1.setRight(k2.getLeft());
-        k2.setLeft(k1);
-        k1.height = Math.max(height(k1.getLeft()), height(k1.getRight())) + 1;
-        k2.height = Math.max(height(k2.getRight()), k1.height) + 1;
-        return k2;
-
-    }
-
-    protected Node doubleWithLeftChild( Node k3 ) {
-        k3.setLeft(rotateWithRightChild( k3.getLeft() ));
-        return rotateWithLeftChild( k3 );
-    }
-
-    protected Node doubleWithRightChild( Node k1 ) {
-        k1.setRight(rotateWithLeftChild( k1.getRight() ));
-        return rotateWithRightChild( k1 );
-    }
-
-
-//    todo filled it up with false
-    public void delete(String toDelete){
-        delete(root, new Node(false, false, false, false, toDelete));
-    }
-
-    private void delete(Node root, Node toDelete){
-        super.deleteRec(root, toDelete);
-        balance(root);
-    }
-
-
-
-    private static final int ALLOWED_IMBALANCE = 1;
-    // Assume t is either balanced or within one of being balanced
-    private Node balance( Node t ){
-        if( t == null ){
-            return t;
-        }
-        if( height( t.getLeft() ) - height( t.getRight() ) > ALLOWED_IMBALANCE ){
-            if( height( t.getLeft().getLeft() ) >= height( t.getLeft().getRight() ) ){
-                t = rotateWithLeftChild( t );
-            }
-            else {
-                t = doubleWithLeftChild( t );
-            }
-        }
-        else {
-            if( height( t.getRight() ) - height( t.getLeft() ) > ALLOWED_IMBALANCE ){
-                if( height( t.getRight().getRight() ) >= height( t.getRight().getLeft() ) ){
+        else if( compareResult > 0 )
+        {
+            t.setRight(insert( x, t.getRight() ));
+            if( height( t.getRight() ) - height( t.getLeft() ) == 2 )
+                if( x.compareTo( t.getRight()) > 0 )
                     t = rotateWithRightChild( t );
-                }
-                else {
+                else
                     t = doubleWithRightChild( t );
-                }
-            }
         }
+        else
+            ;  // Duplicate; do nothing
         t.height = Math.max( height( t.getLeft() ), height( t.getRight() ) ) + 1;
         return t;
     }
 
 
+    private int height( Node t )
+    {
+        return t == null ? -1 : t.height;
+    }
+
+    private Node rotateWithLeftChild( Node k2 )
+    {
+        Node k1 = k2.getLeft();
+        k2.setLeft(k1.getRight());
+        k1.setRight(k2);
+        k2.height = Math.max( height( k2.getLeft() ), height( k2.getRight() ) ) + 1;
+        k1.height = Math.max( height( k1.getLeft() ), k2.height ) + 1;
+        return k1;
+    }
+
+    private Node rotateWithRightChild( Node k1 )
+    {
+        Node k2 = k1.getRight();
+        k1.setRight(k2.getLeft());
+        k2.setLeft(k1);
+        k1.height = Math.max( height( k1.getLeft() ), height( k1.getRight() ) ) + 1;
+        k2.height = Math.max( height( k2.getRight() ), k1.height ) + 1;
+        return k2;
+    }
 
 
+    private Node doubleWithLeftChild(Node k3 ) {
+        k3.setLeft(rotateWithRightChild( k3.getLeft() ));
+        return rotateWithLeftChild( k3 );
+    }
 
-    /* todo check this again without the if node== null*/
-
-
-
+    private Node doubleWithRightChild(Node k1 ) {
+        k1.setRight(rotateWithLeftChild( k1.getRight() ));
+        return rotateWithRightChild( k1 );
+    }
 
     public static void main(String[] args){
         AVLTree tree = new AVLTree();
 
-//        String cases = "RL";
-//
-//        if (cases.equals("LL")){
-//            tree.insert("C");
-//            tree.insert("B");
-//            tree.insert("A");
-//
-//
-////            tree.insert("0");
-////            tree.insert("-1");
-////            tree.insert("-2");
-////            tree.insert("-3");
-//
-//
-//        }
-//
+        String cases = "LL";
+
+        if (cases.equals("LL")){
+            tree.insertAVL(true, true, true, true,"C");
+            tree.insertAVL(true, true, true, true, "B");
+            tree.insertAVL(true, true, true, true, "A");
+
+
+//            tree.insert("0");
+//            tree.insert("-1");
+//            tree.insert("-2");
+//            tree.insert("-3");
+
+
+        }
+
 //        else if (cases.equals("RR")){
-//            tree.insert("A");
-//            tree.insert("B");
-//            tree.insert("C");
+//            tree.insertAVL("A");
+//            tree.insertAVL("B");
+//            tree.insertAVL("C");
 //
 ////            tree.insert("4");
 ////            tree.insert("5");
@@ -180,31 +113,31 @@ public class AVLTree extends BST{
 //        }
 //
 //        else if (cases.equals("LR")){
-//            tree.insert("C");
-//            tree.insert("A");
-//            tree.insert("B");
+//            tree.insertAVL("C");
+//            tree.insertAVL("A");
+//            tree.insertAVL("B");
 ////            tree.insert("5");
 ////            tree.insert("4");
 //        }
 //        else if (cases.equals("RL")){
-//            tree.insert("A");
-//            tree.insert("C");
-//            tree.insert("B");
+//            tree.insertAVL("A");
+//            tree.insertAVL("C");
+//            tree.insertAVL("B");
 //        }
 //        else {
 //            System.out.println("hit the else case!");
 //        }
-//
-//
-//        tree.insert("Y");
-//        tree.insert("D");
-//        tree.insert("T");
-//        tree.insert("H");
-//
-//        System.out.println(tree.find("A"));
-//        System.out.println(tree.find("B"));
-//        System.out.println(tree.find("C"));
-//
+
+
+//        tree.insertAVL("Y");
+//        tree.insertAVL("D");
+//        tree.insertAVL("T");
+//        tree.insertAVL("H");
+
+        System.out.println(tree.find("A"));
+        System.out.println(tree.find("B"));
+        System.out.println(tree.find("C"));
+
 //        System.out.println(tree.find("Y"));
 //        System.out.println(tree.find("D"));
 //        System.out.println(tree.find("T"));
@@ -227,10 +160,6 @@ public class AVLTree extends BST{
 //        System.out.println(tree.find("D"));
 //        System.out.println(tree.find("T"));
 //        System.out.println(tree.find("H"));
-
-
-
-
 
     }
 
